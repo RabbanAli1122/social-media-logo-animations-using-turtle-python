e4bac2bf094ece8928fe039b0a965c7b88410904
Rabban Ali <105205500+RabbanAli1122@users.noreply.github.com>
2022-09-20 14:19:29 +0500

    Add files via upload

---
+++ Source_Code_Of_Linkedin_Logo_Animation.py
@@ -0,0 +1,119 @@
+from turtle import *
+# -----------------------------------------
+#               LOGO BOX CODE
+# -----------------------------------------
+color("#0073B4")
+begin_fill()
+left(90)
+forward(95)
+circle(8,90)
+forward(95)
+circle(8,90)
+forward(95)
+circle(8,90)
+forward(95)
+circle(8,90)
+end_fill()
+
+#---------------------------------------
+#       SETTING POSITION FOR "i"
+#---------------------------------------
+
+color("#0073B4")
+begin_fill()
+left(90)
+forward(90)
+right(90)
+forward(10)
+end_fill()
+
+
+#---------------------------------------------
+#     CODE OF MAKING "i"
+#---------------------------------------------
+
+color("#FFFFFF")
+begin_fill()
+forward(50)
+right(90)
+forward(13)
+right(90)
+forward(50)
+right(90)
+forward(13)
+end_fill()
+
+
+#-----------------------------------------
+#   SETTING LOCATION FOR "."  of "i"
+#-----------------------------------------
+
+color("#0073B4")
+begin_fill()
+right(90)
+forward(55)
+right(90)
+end_fill()
+
+color("#0073B4")
+begin_fill()
+forward(8)
+end_fill()
+
+#----------------------------------------
+#    CODE OF MAKING "." OF "i"
+#________________________________________
+
+color("#FFFFFF")
+begin_fill()
+circle(10)
+end_fill()
+
+
+#----------------------------------------
+#    SETTING LOCATION FOR "n"
+#----------------------------------------
+
+color("#0073B4")
+begin_fill()
+right(90)
+forward(4)
+left(90)
+forward(20)
+right(90)
+end_fill()
+
+#----------------------------------------
+#      CODE OF MAKING OF "n"
+#----------------------------------------
+
+color("#FFFFFF")
+begin_fill()
+forward(50)
+left(90)
+forward(13)
+left(90)
+forward(30)
+circle(-10,180)
+forward(30)
+left(90)
+forward(13)
+left(90)
+forward(30)
+circle(20,130)
+right(130)
+forward(5)
+left(90)
+forward(13)
+end_fill()
+
+#---------------------------------
+#           ENDING
+#---------------------------------
+
+color("#0073B4")
+begin_fill()
+right(90)
+forward(10)
+end_fill()
+done()
---
+++ Source_Code_Of_YouTube_Logo_Animation.py
@@ -0,0 +1,54 @@
+from turtle import *
+
+#-------------------------------------------
+#    CODE OF MAKING THE OUTUR SHELL
+#-------------------------------------------
+
+color("#FE0000")
+begin_fill()
+forward(70)
+circle(30,90)
+forward(70)
+circle(30,90)
+forward(150)
+circle(30,90)
+forward(70)
+circle(30,90)
+forward(80)
+end_fill()
+
+#---------------------------------------------------
+#  CODE OF SETTING THE LOCAION TO MAKE THE TRIANGLE
+#---------------------------------------------------
+
+color("#FE0000")
+begin_fill()
+left(90)
+forward(35)
+left(90)
+forward(20)
+end_fill()
+
+#--------------------------------------------------
+#        CODE OF MAKING THE TRIANGLE
+#---------------------------------------------------
+
+color("#FFFFFF")
+begin_fill()
+right(90)
+forward(60)
+left(-120)
+forward(60)
+right(120)
+forward(60)
+end_fill()
+
+#------------------------------------
+#        ENDING
+#------------------------------------
+
+color("#FE0000")
+begin_fill()
+forward(20)
+end_fill()
+done()
